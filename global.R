376377b54f6c6ab0a2eebbb0783e365421a5f431
Eli Ramthun <duckroller@gmail.com>
2021-01-22 13:06:32 -0600

    folder reorganization
    moved data to its own directory, updated global.R to find it in its new location

--- global.R
+++ global.R
@@ -21,13 +21,13 @@ us_states <- map_data("state")
 us_states$region <- toTitleCase(us_states$region)
 
 # co2 emissions data
-state_emissions_2017 <- read_excel("co2_emissions_profile_by_state.xlsx", skip = 4, n_max = 51,col_names = c("state", "commercial_tons", "electric_power_tons", "residential_tons",	"industrial_tons",	"transportation_tons", "total_tons", "blank", "commercial_pct",	"electric_power_pct",	"residential_pct",	"industrial_pct",	"transportation_pct" 
+state_emissions_2017 <- read_excel("data/co2_emissions_profile_by_state.xlsx", skip = 4, n_max = 51,col_names = c("state", "commercial_tons", "electric_power_tons", "residential_tons",	"industrial_tons",	"transportation_tons", "total_tons", "blank", "commercial_pct",	"electric_power_pct",	"residential_pct",	"industrial_pct",	"transportation_pct" 
 ))
 state_emissions_2017 <- state_emissions_2017[-9,]
 state_emissions_2017$blank <- NULL
 
 # population data
-pop <- read_csv("nst-est2019-alldata.csv", col_types = cols_only(NAME = "c", POPESTIMATE2017 = "i"))
+pop <- read_csv("data/nst-est2019-alldata.csv", col_types = cols_only(NAME = "c", POPESTIMATE2017 = "i"))
 pop$state <- pop$NAME
 pop$NAME <- NULL
 pop$population <- pop$POPESTIMATE2017
@@ -35,10 +35,10 @@ pop$POPESTIMATE2017 <- NULL
 pop <- pop[-c(1:5,14,57),]
 
 # gdp data
-gdp <- read_excel("gdp_by_state_2017_bea.xls", range = "B8:C58", col_names = c("state", "gdp"))
+gdp <- read_excel("data/gdp_by_state_2017_bea.xls", range = "B8:C58", col_names = c("state", "gdp"))
 
 # lat long data points for each state
-state_lat_long <- read_csv("world_country_and_usa_states_latitude_and_longitude_values.csv", 
+state_lat_long <- read_csv("data/world_country_and_usa_states_latitude_and_longitude_values.csv", 
                            col_types = cols(country = col_skip(), 
                                             country_code = col_skip(), latitude = col_skip(), 
                                             longitude = col_skip(), usa_state_code = col_skip()))
@@ -48,12 +48,12 @@ state_lat_long$state <- state_lat_long$usa_state
 state_lat_long$usa_state <- NULL
 
 # partisan lean data
-partisan_lean <- read_csv("fivethirtyeight_partisan_lean_STATES.csv")
+partisan_lean <- read_csv("data/fivethirtyeight_partisan_lean_STATES.csv")
 partisan_lean <- partisan_lean[-c(2:4,6)]
 partisan_lean$rep_lean <- -partisan_lean$dem_lean
 
 # electricity price data
-avgprice_annual <- read_excel("avgprice_annual.xlsx", skip = 1)
+avgprice_annual <- read_excel("data/avgprice_annual.xlsx", skip = 1)
 avgprice_annual <- avgprice_annual[avgprice_annual$Year == 2017,]
 avgprice_annual <- avgprice_annual[avgprice_annual$`Industry Sector Category` =="Total Electric Industry",]
 avgprice_annual$state <- state.name[match(avgprice_annual$State,state.abb)]
@@ -61,7 +61,7 @@ avgprice_annual <- avgprice_annual[-c(1:3,8)]
 colnames(avgprice_annual) <-  c("Residential Price", "Commercial Price", "Inudstrial Price", "Transportation Price", "Total Price", "state")
 
 # electricity source data
-annual_generation_state <- read_excel("annual_generation_state.xls", skip = 1)
+annual_generation_state <- read_excel("data/annual_generation_state.xls", skip = 1)
 annual_generation_state <- annual_generation_state[annual_generation_state$YEAR == 2017,]
 annual_generation_state <- annual_generation_state[annual_generation_state$`TYPE OF PRODUCER`=="Total Electric Power Industry",]
 annual_generation_state$state <- state.name[match(annual_generation_state$STATE,state.abb)]
@@ -73,9 +73,9 @@ annual_generation_state[is.na(annual_generation_state)] <- 0
 colnames(annual_generation_state) <- c("state", "coal_mwh", "geothermal_mwh", "hydro_mwh", "nat_gas_mwh", "nuclear_mwh", "other_mwh", "other_biomass_mwh", "other_gases_mwh", "petroleum_mwh", "pumped_storage_mwh", "solar_mwh", "total_mwh", "wind_mwh", "wood_biomass_mwh")
 
 # climate data
-hdd <- read_excel("2017_statelevel_climate_data.xlsx", 
+hdd <- read_excel("data/2017_statelevel_climate_data.xlsx", 
                   sheet = "HDD_plaintext")
-cdd <- read_excel("2017_statelevel_climate_data.xlsx", 
+cdd <- read_excel("data/2017_statelevel_climate_data.xlsx", 
                   sheet = "CDD_plaintext")
 
 hdd <- hdd[,c(1,14)]
